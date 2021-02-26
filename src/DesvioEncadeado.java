7cd8a0070d1ad78a805a62049813aa34e4e27273
Lucas Oliveira <lucascardoso1992@hotmail.com>
2021-02-26 19:58:16 -0300

    If composto e encadeado

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -7,14 +7,24 @@ public class DesvioEncadeado {
 		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
 		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
 		
+		//Scanner registra os comandos executados no teclado.
 		Scanner leitor = new Scanner(System.in);
 		int idade;
 		System.out.println("Por favor, digite a idade do passageiro.");
 		idade = leitor.nextInt();
 	
 		/*Aqui devemos criar uma solução para realizar as verificações.*/
+		if (idade <16) {
+		System.out.println("Essa pessoa não pode votar e não pode embarcar");
+		}
 		
+		else if (idade < 18) {
+		System.out.println("Tanto a votação quanto o embarque são opcionais para essa pessoa");
+		}
 		
+		else {
+		System.out.println("Essa pessoa é obrigada a votar");
+		}
 	
 		leitor.close();
 
